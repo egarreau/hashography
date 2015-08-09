eb17e16d68ed2aa288003c39e6536cb4df582851
Apprentice <tra38@nau.edu>
2015-08-09 15:12:44 -0500

    Display all 'geocoded' coordinates onto the Google Map

--- public/javascript/geocoder.js
+++ public/javascript/geocoder.js
@@ -1,26 +1,29 @@
 var geocoder = new google.maps.Geocoder();
 
-socket.on('geocoder', function(data){
-  var address = data.location
-  if (address === "")
-  {
-    console.log("Blank string")
-  }
-  else {
-  setTimeout(geocoding(address), 500);
-  };
-});
 
-var geocoding = function(address) {
-  geocoder.geocode({"address": address}, function(results, status){
-    if (status == google.maps.GeocoderStatus.OK){
-      var location = results[0].geometry.location
-      var lat = location.G
-      var lng = location.K
-      console.log([lat, lng]);
+$(document).ready(function(){
+    socket.on('geocoder', function(data){
+    var address = data.location
+    if (address === "")
+    {
+      console.log("Blank string")
     }
     else {
-      console.log("Geocode was not successful for the following reason: "+ status)
-    }
-  })
-};
+    setTimeout(geocoding(address), 500);
+    };
+  });
+
+  var geocoding = function(address) {
+    geocoder.geocode({"address": address}, function(results, status){
+      if (status == google.maps.GeocoderStatus.OK){
+        var location = results[0].geometry.location
+        var lat = location.G
+        var lng = location.K
+        makeMarker([lat, lng], map);
+      }
+      else {
+        console.log("Geocode was not successful for the following reason: "+ status)
+      }
+    })
+  };
+});
