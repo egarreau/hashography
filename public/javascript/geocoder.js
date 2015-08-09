228b7b5fceeaca769d1a2709c66504bcaec5fc25
Apprentice <tra38@nau.edu>
2015-08-09 14:51:50 -0500

    Pass location data to Google API and retreive coordinate points

--- public/javascript/geocoder.js
+++ public/javascript/geocoder.js
@@ -1,1 +1,26 @@
-socket.on('geocoder')
+var geocoder = new google.maps.Geocoder();
+
+socket.on('geocoder', function(data){
+  var address = data.location
+  if (address === "")
+  {
+    console.log("Blank string")
+  }
+  else {
+  setTimeout(geocoding(address), 500);
+  };
+});
+
+var geocoding = function(address) {
+  geocoder.geocode({"address": address}, function(results, status){
+    if (status == google.maps.GeocoderStatus.OK){
+      var location = results[0].geometry.location
+      var lat = location.G
+      var lng = location.K
+      console.log([lat, lng]);
+    }
+    else {
+      console.log("Geocode was not successful for the following reason: "+ status)
+    }
+  })
+};
