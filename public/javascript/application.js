82b3ccb132481db193f958e9c8ae7e7c428a82f4
egarreau <ergarreau@gmail.com>
2015-08-08 19:47:59 -0500

    IT WOOOOOORKS

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,64 +1,64 @@
-function makeMarker(lat, lng, map){
-  var marker = new google.maps.Marker({
-    position: { lat: lng, lng: lat },
-    map: map,
-    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQglNi0Ylupo6wQ5jCRFHJ1bI98nTsjv1oW_o8s8IaysGhQVHKzcnx1pK4'
-  })
-}
+// function makeMarker(lat, lng, map){
+//   var marker = new google.maps.Marker({
+//     position: { lat: lng, lng: lat },
+//     map: map,
+//     icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQglNi0Ylupo6wQ5jCRFHJ1bI98nTsjv1oW_o8s8IaysGhQVHKzcnx1pK4'
+//   })
+// }
 
 
-var Twitter = require('twitter');
+// var Twitter = require('twitter');
 
-var client = new Twitter({
-  consumer_key: 'ncjKabjKKdcmnKkTyA0irhjIG',
-  consumer_secret: '63XT0n4AdCcad3v3g37fHwelbbekHERNFVgC5GJm7HWQJoXvg4',
-  access_token_key: '127879897-dSW0TRtfjE0eWYHyf0VHtH6UDNDFRCX1G8afJETN',
-  access_token_secret: 'Rej1phiRJzAi62ah6c2hrSPmlTQczzhY2ctwoP1ckmMfh'
-});
-
-
-var streamData = function(searchTerm){
-  client.stream('statuses/filter', {track: searchTerm}, function(stream){
-    stream.on('data', function(tweet) {
-      console.log("Text: " + tweet.text);
-
-      if (tweet.coordinates === null || tweet.coordinates === undefined) {
-        if (tweet.place === null){
-          // parse tweet.user.location using CSV matching
-        }
-        else{
-          // send tweet.place.bounding_box.coordinates to map
-          tweet.place.bounding_box.coordinates;
-        };
-      }
-      else{
-        // send tweet.coordinates.coordinates to map
-        tweet.coordinates.coordinates;
-      };
-
-    });
-
-  });
-};
-
-$(document).ready(function(){
-  var mapOptions = {
-    center: { lat: 20, lng: 0},
-    zoom: 3
-  };
-debugger;
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  var midX = (22.2694931 + 22.9559184) / 2
-  var midY = (88.0944569 + 88.6100265) / 2
-  makeMarker(88.0944569, 22.2694931, map);
-  makeMarker(88.0944569, 22.9559184, map);
-  makeMarker(88.6100265, 22.9559184, map);
-  makeMarker(88.6100265, 22.2694931, map);
-  makeMarker(midY, midX, map);
-
-  // 88.0944569,22.2694931,88.0944569,22.9559184,88.6100265,22.9559184,88.6100265,22.2694931
+// var client = new Twitter({
+//   consumer_key: 'ncjKabjKKdcmnKkTyA0irhjIG',
+//   consumer_secret: '63XT0n4AdCcad3v3g37fHwelbbekHERNFVgC5GJm7HWQJoXvg4',
+//   access_token_key: '127879897-dSW0TRtfjE0eWYHyf0VHtH6UDNDFRCX1G8afJETN',
+//   access_token_secret: 'Rej1phiRJzAi62ah6c2hrSPmlTQczzhY2ctwoP1ckmMfh'
+// });
 
 
-})
+// var streamData = function(searchTerm){
+//   client.stream('statuses/filter', {track: searchTerm}, function(stream){
+//     stream.on('data', function(tweet) {
+//       console.log("Text: " + tweet.text);
+
+//       if (tweet.coordinates === null || tweet.coordinates === undefined) {
+//         if (tweet.place === null){
+//           // parse tweet.user.location using CSV matching
+//         }
+//         else{
+//           // send tweet.place.bounding_box.coordinates to map
+//           tweet.place.bounding_box.coordinates;
+//         };
+//       }
+//       else{
+//         // send tweet.coordinates.coordinates to map
+//         tweet.coordinates.coordinates;
+//       };
+
+//     });
+
+//   });
+// };
+
+// $(document).ready(function(){
+//   var mapOptions = {
+//     center: { lat: 20, lng: 0},
+//     zoom: 3
+//   };
+// debugger;
+//   var map = new google.maps.Map(document.getElementById('map-canvas'),
+//       mapOptions);
+
+//   var midX = (22.2694931 + 22.9559184) / 2
+//   var midY = (88.0944569 + 88.6100265) / 2
+//   makeMarker(88.0944569, 22.2694931, map);
+//   makeMarker(88.0944569, 22.9559184, map);
+//   makeMarker(88.6100265, 22.9559184, map);
+//   makeMarker(88.6100265, 22.2694931, map);
+//   makeMarker(midY, midX, map);
+
+//   // 88.0944569,22.2694931,88.0944569,22.9559184,88.6100265,22.9559184,88.6100265,22.2694931
+
+
+// })
